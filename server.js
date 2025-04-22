b22742d3ac6a1c1b9fd1925971c227f5647f982b
shash-camp <shashank221405@gmail.com>
2025-04-23 04:15:47 +0530

    thirdd commit

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
 // STATIC FOLDER
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/dist")));
 
 // STATIC ROUTES
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
 });
 
 //port
