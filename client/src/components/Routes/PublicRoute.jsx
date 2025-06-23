b4e207e36c8f169c250ce44d6c1d3a4f545bdf78
Shashank Singh <163159694+shash-camp@users.noreply.github.com>
2025-06-23 17:47:18 +0530

    Update PublicRoute.jsx

--- client/src/components/Routes/PublicRoute.jsx
+++ client/src/components/Routes/PublicRoute.jsx
@@ -1,9 +1,26 @@
+// import React from "react";
+// import { Navigate } from "react-router-dom";
+
+// const PublicRoute = ({ children }) => {
+//   if (localStorage.getItem("token")) {
+//     return <Navigate to="/" />;
+//   } else {
+//     return children;
+//   }
+// };
+
+// export default PublicRoute;
+
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 const PublicRoute = ({ children }) => {
-  if (localStorage.getItem("token")) {
+  const location = useLocation();
+
+  // Redirect to home if token exists and trying to access login or register
+  if (localStorage.getItem("token") && (location.pathname === "/login" || location.pathname === "/register")) {
     return <Navigate to="/" />;
+
   } else {
     return children;
   }
